52e904817c69b32e5efae9b407de71428d470995
EliasAlmen <almen.elias@gmail.com>
2023-12-17 20:27:34 +0100

    Add project files.

---
+++ MauiColorPicker/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiColorPicker
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+    }
+
+}
